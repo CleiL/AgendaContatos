41bf181f9fcf4e1ab9e64e55dfae1d0ad17ed5e2
CleiL <clei.lisboa@gmail.com>
2022-10-08 23:26:42 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("formulario")
+let linhas = '';
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const inputNomeContato = document.getElementById('nome');
+    const inputNumeroContato = document.getElementById('numero');
+
+
+    let linha = '<tr>';
+    linha +=`<td> ${inputNomeContato.value} </td>`;
+    linha +=`<td> ${inputNumeroContato.value} </td>`; 
+    linha += `</tr>`;
+    
+    linhas += linha;
+
+    const agendatabela = document.querySelector('tbody');
+    agendatabela.innerHTML = linhas;
+
+});
+
+
